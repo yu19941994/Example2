d23e64fb6f187a3ed402908d590d8904c23274b7
yishian yu <yishian.yu@gmail.com>
2018-10-15 15:00:55 +0800

    example1

---
+++ Assets/Earth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : MonoBehaviour {
+
+    public float y;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform transform = GetComponent<Transform>();
+        //Vector3 pos = transform.position;
+        //pos.x = pos.x + 0.02f;
+        //transform.position = pos;
+        Vector3 angle = new Vector3(0, y * Time.deltaTime, 0);
+        transform.Rotate(angle);
+
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        DateTime date = DateTime.Now;
+        //Debug.Log(date.ToString());
+        GetComponent<TextMesh>().text = date.ToString();
+    }
+}
